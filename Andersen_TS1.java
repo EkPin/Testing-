d48417302bc6511ddbbf35705a8153e8dcf83d22
Ekaterina <ek.pin@bk.ru>
2021-08-21 21:19:37 +0300

    Andersen_TS1

---
+++ Andersen_TS1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Andersen_TS1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        number = scanner.nextInt();
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+    }
+}
+
